b8b062ca5d01181fb9867a85f98f503e4b2aa9d6
Step Guest <stepguest@ad.itstep.az>
2024-09-10 19:57:28 +0400

    Add RabbitMQ

--- Controllers/IdentityController.cs
+++ Controllers/IdentityController.cs
@@ -1,16 +1,20 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using IdentityMS.Data;
 using IdentityMS.Models;
 using Microsoft.AspNetCore.Mvc;
+using RabbitMQ.Client;
 
 namespace IdentityMS.Controllers;
 [ApiController]
 public class IdentityController : ControllerBase
 {
     private readonly IdentityMsDbContext dbContext;
+    private readonly ConnectionFactory rabbitMqConnectionFactory;
 
     public IdentityController(IdentityMsDbContext dbContext)
     {
@@ -18,18 +22,39 @@ public class IdentityController : ControllerBase
     }
 
     [HttpPost("api/[controller]/[action]")]
-    public Task<IActionResult> SignUp(User user)
+    public async Task<IActionResult> SignUp(User user)
     {
         try
         {
             this.dbContext.Users.Add(user);
-            base.Created();
+
+            using var connection = this.rabbitMqConnectionFactory.CreateConnection();
+            using var channel = connection.CreateModel();
+            var destination = "rabbit_mq";
+
+            var result = channel.QueueDeclare(
+                queue: destination,
+                durable: true,
+                exclusive: false,
+                autoDelete: false
+            );
+
+            var userJson = JsonSerializer.Serialize(user);
+
+            var messageInBytes = Encoding.UTF8.GetBytes(userJson);
+
+            channel.BasicPublish(
+                exchange: string.Empty,
+                routingKey: destination,
+                basicProperties: null,
+                body: messageInBytes
+            );
+
+            return base.Created();
         }
         catch (Exception ex)
         {
-            base.BadRequest(ex.Message);
+            return base.BadRequest(ex.Message);
         }
-
-        base.StatusCode(500);
     }
 }
